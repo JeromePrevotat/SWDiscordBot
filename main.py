fe0e8db0af8a0c24ac8325a523c9e666a2bf4bc7
Prévotat Jérôme <jer.prevotat@gmail.com>
2021-01-29 13:29:55 +0100

    Pazaak Models done Began working on bot cmds

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+###############################################################################
+#                         IMPORTS                                             #
+###############################################################################
+
 import os
 import discord
+import inspect
 from dotenv import load_dotenv
 from discord.ext import commands
 
+###############################################################################
+#                         CONSTANTS                                           #
+###############################################################################
+
 load_dotenv(dotenv_path="./.env/config")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+###############################################################################
+#                         CLASSES                                             #
+###############################################################################
 
 class KhaBot(commands.Bot):
     """KhaBot class to dominate Discord."""
@@ -15,6 +28,16 @@ class KhaBot(commands.Bot):
         self._intents.members=True
         self.chan = None;
 
+        # Activates all Command instances of the Bot
+        members = inspect.getmembers(self)
+        for name, member in members:
+            if isinstance(member, commands.Command):
+                if member.parent is None:
+                    self.add_command(member)
+
+    # On connection
+    # Find the khabot channel and check if the Bot has r/w permissions on it
+    # Then send a message to the channel when ready to interact with users
     async def on_ready(self):
         gen = self.get_all_channels()
         for chan in gen:
@@ -28,10 +51,14 @@ class KhaBot(commands.Bot):
                     await chan.send(f"{self.user.display_name} has awoken !")
                     break;
 
-    async def on_message(self, message):
-        if (message.author.id != self.user.id and message.channel == self.chan):
-            if (message.content.lower() == "kha"):
-                await message.channel.send("Tar")
+    # Delete the last 20 messages in the Bot channel and close the Bot if Owner
+    @commands.is_owner()
+    @commands.command()
+    async def die(ctx):
+        messagesList = await ctx.bot.chan.history(limit=20).flatten()
+        await ctx.bot.chan.delete_messages(messagesList)
+        await ctx.bot.logout()
 
-khabot = KhaBot()
-khabot.run(BOT_TOKEN)
+if __name__ == "__main__":
+    khabot = KhaBot()
+    khabot.run(BOT_TOKEN)
--- pazaak.py
+++ pazaak.py
@@ -1,84 +1,129 @@
-from random import randint
+###############################################################################
+#                         IMPORTS                                             #
+###############################################################################
 
-BASIC_CARD = 0
-HAND_CARD = 1
-FLIP_CARD = 2
-DOUBLE_CARD = 3
+from random import shuffle
+
+###############################################################################
+#                         CONSTANTS                                           #
+###############################################################################
+
+HAND_CARDS = {
+    # Basic Cards
+    "-1": ("-","1"),        "+1": ("+","1"),
+    "+/-1": ("+/-","1"),    "-/+1": ("-/+","1"),
+    "-2": ("-","2"),        "+2": ("+","2"),
+    "+/-2": ("+/-","2"),    "-/+2": ("-/+","2"),
+    "-3": ("-","3"),        "+3": ("+","3"),
+    "+/-3": ("+/-","3"),    "-/+3": ("-/+","3"),
+    "-4": ("-","4"),        "+4": ("+","4"),
+    "+/-4": ("+/-","4"),    "-/+4": ("-/+","4"),
+    "-5": ("-","5"),        "+5": ("+","5"),
+    "+/-5": ("+/-","5"),    "-/+5": ("-/+","5"),
+    "-6": ("-","6"),        "+6": ("+","6"),
+    "+/-6": ("+/-","6"),    "-/+6": ("-/+","6"),
+
+    # Flip Cards & Double
+    "2/4": ("f","2/4"),     "4/2": ("f","4/2"),
+    "3/6": ("f","3/6"),     "6/3": ("f","6/3"),
+    "x2": ("x","2"),
+}
+
+SIGN = ["-","-/+","+","+/-","x","f"]
+
+DEFAULT_DECK_SIZE = 40
+
+###############################################################################
+#                         CLASSES                                             #
+###############################################################################
 
 class Card(object):
     """Pazaak Card Class."""
 
-    def __init__(self, cardtype, value: int, sign: str, special: str):
+    def __init__(self, value):
         self.value = value
-        self.sign = sign
-        self.cardtype = cardtype
-        self.special = special
-        if self.cardtype == BASIC_CARD or self.cardtype == HAND_CARD:
-            self.stringvalue = self.sign + str(self.value)
-        else:
-            self.stringvalue = self.special
+        self.stringvalue = str(self.value)
 
-    def __new__(cls, cardtype, value: int, sign: str, special: str):
-        if (cardtype == BASIC_CARD and (sign != "+")):
-            raise ValueError
-        if (cardtype == HAND_CARD
-        and ((value < 1 or value > 6) or sign not in ["-","-/+","+","+/-"])):
-            raise ValueError
-        if (cardtype == FLIP_CARD
-        and ((value != 0) or special not in ["2/4","4/2","3/6","6/3"])):
-            raise ValueError
-        if (cardtype == DOUBLE_CARD and ((value != 0) or special != "double")):
-            raise ValueError
+    def __new__(cls, value):
+        if int(value) < 1 or int(value) > 10: raise ValueError("Invalid Card Value.")
+        return object.__new__(cls)
+
+    def __str__(self):
+        return str(self.value)
+
+
+class HandCard(object):
+    """Pazaak Hand Card Class."""
+
+    def __init__(self, sign, value):
+        self.sign = sign
+        self.value = value
+        self.stringvalue = self.sign + str(self.value)
+
+    def __new__(cls, sign, value):
+        if sign is not None and sign not in SIGN: raise ValueError(f"Invalid Card Sign {sign}.")
+        accepted_values = []
+        for k,v in HAND_CARDS.items():
+            accepted_values.append(v[1])
+        accepted_values = list(dict.fromkeys(accepted_values))
+        if value not in accepted_values: raise ValueError(f"Invalid Card Value {value}.")
         return object.__new__(cls)
 
     def __str__(self):
         return self.stringvalue
 
-def BuildCard(cardtype, value=None, sign="+", special=""):
-    if value is None:
-        value = randint(1, 10)
-    try:
-        newCard = Card(cardtype,value,sign,special)
-    except ValueError:
-        print(ValueError)
-    return newCard
 
-def BuildDeck():
-    deck = []
-    cards = 0
-    while cards < 40:
-        newCard = BuildCard(BASIC_CARD)
-        nb_card = 0
-        for card in deck:
-            if card.stringvalue == newCard.stringvalue:
-                nb_card += 1
-        if nb_card < 4:
-            deck.append(newCard)
-            cards += 1
-    return deck
+class Deck(object):
+    """Pazaak Deck Class."""
 
-def BuildHand(cardlist):
-    sidedeck = []
-    for c in cardlist:
-        if c == "double":
-            sidedeck.append(BuildCard(DOUBLE_CARD,0,special="double"))
-        elif c in ["2/4","4/2","3/6","6/3"]:
-            sidedeck.append(BuildCard(FLIP_CARD,0,special=c))
-        else:
-            sidedeck.append(BuildCard(HAND_CARD,int(c[1]),c[0]))
-    return sidedeck
+    def __init__(self, cards_qty=DEFAULT_DECK_SIZE, cardlist=None):
+        if cards_qty is None or cards_qty <= 0: raise ValueError("Invalid number of Cards.")
+        if cardlist is not None and len(cardlist) != cards_qty:
+            raise ValueError("The number of cards does not match the number of Cards from the card list.")
 
-def BuildSideDeck(cardstring):
+        self.cards = []
+
+        # Create a Dealer Deck
+        value = 0
+        while cards_qty > 0:
+            if cardlist is None:
+                if cards_qty % 4 == 0:
+                    value += 1
+                self.cards.append(Card(value))
+            # Create a Side Deck or a Hand
+            elif cardlist is not None:
+                    self.cards.append(HandCard(cardlist[cards_qty-1][0], cardlist[cards_qty-1][1]))
+            cards_qty -= 1
+
+        # Shuffle the Deck
+        shuffle(self.cards)
+
+        self.stringvalue = ""
+        for c in self.cards:
+            self.stringvalue += c.stringvalue + ";"
+
+    def __str__(self):
+        return self.stringvalue.strip()
+
+###############################################################################
+#                         FUNCTIONS                                           #
+###############################################################################
+
+def sanitizeCardlist(cardstring):
     cardlist = cardstring.lower().split(";")
     cardlist = [c.strip() for c in cardlist]
     cardlist = [c for c in cardlist if c]
-    cardlist = [(c[:-1],c[-1]) if (c.lower() not in ["2/4","4/2","3/6","6/3","double"]) else (c) for c in cardlist]
-    hand = BuildHand(cardlist)
-    return hand
+    cleaned = []
+    for c in cardlist:
+        if c not in HAND_CARDS.keys():
+            raise ValueError(f"Invalid card: {c}.")
+        else:
+            cleaned.append(HAND_CARDS[c])
+    return cleaned
 
 if __name__ == "__main__":
-    deck = BuildDeck()
-    print("Deck Builded !")
-    sidedeck="   +/-3; +5; -4;-/+2;  ; double;2/4"
-    hand = BuildSideDeck(sidedeck)
-    print("Hand Builded !")
+    deck = Deck()
+    sidedeck_string = "+6;-6;+/-6;-/+6;2/4; x2;4/2 ;  +1  ; -1 ; +/-2"
+    cardlist = sanitizeCardlist(sidedeck_string)
+    sidedeck = Deck(10,cardlist)
+    print(sidedeck)
