5f50223ced7462f3332e8f235106e8e06b91c17c
Prévotat Jérôme <jer.prevotat@gmail.com>
2021-06-07 19:17:43 +0200

    - Implemented Logs Testing

--- src/tests/test_logsfile.py
+++ src/tests/test_logsfile.py
@@ -5,7 +5,9 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
 import unittest
+import datetime
 
 import khabot
 
@@ -16,43 +18,68 @@ import khabot
 class TestLogsFile(unittest.TestCase):
     """Tests Logs file CRUD."""
 
+    def setUp(self):
+        # Var SetUp
+        self.parentDir = os.path.abspath(os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__))))
+        self.logsDirName = 'logs'
+        self.logsDir = os.path.join(os.path.abspath(os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__)))), self.logsDirName)
+        self.prefix = khabot.bot.PREFIX
+        self.testBot = khabot.bot.KhaBot()
+        self.cmdList = [
+            'effects', 'have', 'local', 'help',
+        ]
+
+    def tearDown(self):
+        self.cleanDir(self.logsDir)
+
     def cleanDir(self, path):
         if os.listdir(path) != []:
             for f in os.listdir(path):
-                print(f)
-                print(path)
                 try:
-                    if os.path.isdir(f):
-                        cleanDir(os.path.join(path, f))
-                    else:
-                        os.remove(f)
+                    if not os.path.isdir(os.path.join(path, f)):
+                        os.remove(os.path.join(path, f))
+                        print('RM FILE: {}\n'.format(os.path.join(path, f)))
                 except OSError as e:
                     print('{}\n -> {}'.format(os.path.join(path, f), e))
-        try:
-            os.rmdir(path)
-        except OSError as e:
-            print('{}\n -> {}'.format(path, e))
 
-    def setUp(self):
-        # Var SetUp
-        self.testBot = khabot.bot.KhaBot()
-        self.fileDir = os.path.dirname(os.path.abspath(__file__))
-        self.logsParentDir = os.path.join(self.fileDir, '..')
-        self.dirName = 'logs'
-        self.logsFileName = ''
-        # Dir SetUp
-        # Clean and Remove Logs Dir if it exists
-        if self.dirName in os.listdir(self.logsParentDir):
-            self.cleanDir(os.path.join(self.logsParentDir, self.dirName))
-
-    def test_dir_create(self):
+    def test_create_logs_dir(self):
         """Tests the correct creation of the logs directory."""
-        self.assertTrue(self.dirName in os.listdir(self.logsParentDir))
+        # Mkdir Here
+        self.assertTrue(self.logsDirName in os.listdir(self.parentDir))
+
+    def test_file_naming(self):
+        """Tests the correct naming of logs files."""
+        self.cleanDir(self.logsDir)
+        correctName = datetime.date.today().isoformat()
+        # Create File Here
+        fileList = os.listdir(self.logsDir)
+        self.assertTrue(fileList[0], correctName)
+
+    def test_file_content(self):
+        """Tests the correct formatting of the log file content."""
+        mine = ''
+        ref = ''
+        # Fill Log File Here
+        for cmd in self.cmdList:
+            header = datetime.datetime.now().time()
+            header = header.isoformat(timespec='seconds') + ': ' + self.prefix
+            if cmd == 'effects':
+                ref +=' '.join([header, cmd, '\n'])
+            if cmd == 'have':
+                ref += ' '.join([header, cmd, 'arg1', 'arg2', '\n'])
+            if cmd == 'local':
+                ref += ' '.join([header, cmd, 'arg1', '\n'])
+            if cmd not in ['effects', 'have', 'local']:
+                ref += ' '.join([header, cmd, '\n'])
+        self.assertEqual(mine, ref)
+
+    def test_delete_old_files(self):
+        today = datetime.date.today()
+        aWeekOld = today - datetime.timedelta(days = 7)
+        # Delete Files Here
+        self.assertEqual(os.listdir(self.logsDir), [])
 
 if __name__ == '__main__':
-    import sys
-    print(sys.path)
-    t = TestLogsFile()
-    p = os.path.join(
-        os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'logs')
-    t.cleanDir(p)
+    unittest.main()
