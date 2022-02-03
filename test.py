e3f40b7ad60ee922cdd69602242418f21da2e844
Prévotat Jérôme <jer.prevotat@gmail.com>
2022-02-03 20:57:23 +0100

    Added health Steal Up Effect

--- test.py
+++ test.py
@@ -11,12 +11,12 @@ import servers_locals
 
 if __name__ == '__main__':
     swgoh = swgohgg.Swgohgg()
-    abilities = swgoh.get_from_api('abilities')
+    charList = swgoh.get_from_api('characters')
     matches = []
-    for charDict in abilities:
-        if charDict['character_base_id'] == 'EZRABRIDGERS3'\
-            and charDict['type'] != 5\
-            and charDict['type'] == 1:
-            matches.append(charDict)
-    for ab in matches:
-        print('{}\n'.format(ab))
+    for character in charList:
+        for ablt in character['ability_classes']:
+            if ablt not in matches:
+                matches.append(ablt)
+    with open('trash', 'w+') as f:
+        for ablt in matches:
+            f.write(ablt + '\n')
