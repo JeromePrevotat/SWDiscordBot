452b6fb5d802c984591a84d99a2effee7f69bec0
Prévotat Jérôme <jer.prevotat@gmail.com>
2022-05-06 15:15:59 +0200

    Update for lastest Conquest 3 first Sectors

--- feats.py
+++ feats.py
@@ -7,58 +7,58 @@ FEATS = {
     '1':{
         # Sector Feats
         'Sector':
-        '**The Guild :** Defeat 50 enemies with Bounty Hunter\n\t- BH + Bando + Boba\n\n\
-        **Unguarded :** Win 5 battles with no Tanks in your Squad\n\t- CLS + Chewpio\n\n\
-        **Anticipated Dodge :** Gain Foresight 60 Times\n\t- Jedi : QGJ Lead(optionnal) + GMY + Hoda\n\n\
-        **Blinding Assault :** Attempt to inflict Blind 40 times\n\t- CLS + Chewpio\n\n',
+        '**Flawless Victory :** Win at least 20 battlew without losing any units\n\t- JML + Wat + JKR + EP + GMY\n\n\
+        **Such a Quiet Thing :** Defeat 50 enemies with Old Republic\n\t- JML + Wat + JKR + EP + GMY/JKR Bastilla Jolee Zaalbar Mission/JML JKR Bastilla Jolee\n\n\
+        **Siphinong Strike :** Gain health Steal Up 40 Times\n\t- JML + Wat + JKR + EP + GMY\n\n\
+        **Incapaciting Strike :** Attempt to inflict Potency Down 40 times\n\t- JML + Wat + JKR + EP + GMY\n\n',
         # Mini Boss Feats
         'Miniboss':
-        '**Mini Boss Dash Rendar & Prepared Crew:**\n\
-        **Fortunate to be All in One Piece :** Win with URoRRuR\'R\'R surviving\n\t- JML + JKL\n\n\
-        **Utinni! :** Win with at least 3 Jawas in your Squad\n\t- JML + JKL\n\n',
+        '**Mini Boss:**\n\
+        **Tomb Raiders :** Win with Cad Bane and Aurra Sing surviving\n\t- JML + Wat + Cad + Aurra + Gas/JKL\n\n\
+        **Amateur Hour :** Win without using any Bounty Hunter in your squad\n\t- Whatever\n\n',
         # Boss Feats
         'Boss':
-        '**Boss JKR Jedi:**\n\
-        **I\'ll Take Those Odds :** Win witht he Mandalorian surviving\n\t- BH/Maul Mandos/JML\n\n\
-        **Amateur Hour :** Win without any Bounty Hunters in your Squad\n\t- Whatever\n\n',
+        '**Boss Ajunta Pall:**\n\
+        **The Sword of Ajunta Pall :** Win with Talia, Nightsister Initiate, and Zaalbar surviving\n\t- JML + Wat + Inititate + Talia + Zaalbar\n\n\
+        **Hokey Religions :** Win without using any Jedi, Sith, or Unaligned Force User in your Squad\n\t- Whatever\n\n',
         },
     # Sector 2
     '2':{
         # Sector Feats
         'Sector':
-        '**Flawless Victory :** Win at least 20 battles without losing any units\n\t- Whatever\n\n\
-        **Armor Up :** Gain Defense Up 60 times\n\t- Jedi + Old Ben + Zariss + GMY\n\n\
-        **All Imperial Troopers :** Win at least 14 battles with a full Squad of Imperial Troopers units\n\t- IT\n\n\
-        **Siphoning Strike :** Gain Health Steal Up 20 times\n\t- EP Lead + Vader + Thrawn vs Mini Boss Traya\n\n',
+        '**Lucky Shot :** Score at least 200 critical hits\n\t- CLS\n\n\
+        **Beep Boop :** Defeat 50 enemies with Droid units\n\t- GG\n\n\
+        **Fight for the Light :** Win at least 14 battles with a full Squad of Light Side units\n\t- CLS\n\n\
+        **Clumsy :** Attempt to inflict Evasion Down 40 times\n\t- CLS\n\n',
         # Mini Boss Feats
         'Miniboss':
-        '**Mini Boss Traya:**\n\
-        **Retreat from Echo Base :** Win with Cpatain Han Solo Surviving\n\t- JML\n\n\
-        **End of the Rebellion :** Win without using any Rebels in your Squad\n\t- EP Lead + Vader + Thrawn\n\n',
+        '**Mini Boss:**\n\
+        **Subject 1138 :** Win with Starkiller Surviving\n\t- F That\n\n\
+        **Hokey Religions :** Win without using any Jedi, Sith, or Unaligned Force User in your Squad\n\t- Whatever\n\n',
         # Boss Feats
         'Boss':
-        '**Boss : GG:**\n\
-        **Target. Maximum Fire Power :** win with general Veers and SnowTrooper surviving\n\t- IT + IT Tech\n\n\
-        **The Empire is Gone :** Win without using any Empire units in your Squad\n\t- Whatever\n\n',
+        '**Boss :**\n\
+        **Mysterious Stranger :** Win with 5 Victors\n\t- Whatever\n\n\
+        **Calculated Risk :** Complete the battle with an undersized Squad\n\t- C3PO + Darth Nihilus + Darth Sion\n\n',
         },
     # Sector 3
     '3':{
         # Sector Feats
         'Sector':
-        '**Lucky Shot :** Score at least 200 Critical Hits\n\t- Whatever\n\n\
-        **All Jedi :** Win at least 14 battles with a full Squad of Jedi units\n\t- Jedi : QGJ Lead(optionnal) + GMY + Hoda\n\n\
-        **Fleet-Footed :** Evade at least 100 attacks in battles you won\n\t- Jedi : QGJ Lead(optionnal) + GMY + Hoda\n\n\
-        **Beep Boop :** Defeat 50 enemies with Droid Units\n\t- GG\n\n',
+        '**Shadowy Dealings :** Gain Stealth 40 times\n\t- JML + Wat + R2D2 + BSF + Inquisitor\n\n\
+        **Reeling Blow :** Attempt to inflict Stagger 60 times\n\t- JML + Wat + R2D2 + BSF + Inquisitor/Dralak\n\n\
+        **Disarmed :** Win 14 battles with no Attacker in your Squad\n\t- JML + Wat + R2D2 + BSF + Inquisitor (5th brother)\n\n\
+        **Hunters One and All :** Defeat 50 enemies with inquisitorius Units\n\t- JML + Wat + R2D2 + BSF + Inquisitor\n\n',
         # Mini Boss Feats
         'Miniboss':
-        '**Mini Boss MM:**\n\
-        **Separatist Leader :** Win with Count dooku and Nute Gunray surviving\n\t- JML JKL Dooku Nute Wat\n\n\
-        **For the Republic :** Win without using any Separatists in your Squad\n\t- Whatever\n\n',
+        '**Mini Boss Dralak:**\n\
+        **Power of the Force :** Win with Jedi Knight Revan and Bastilla Shan surviving\n\t- JKR + Bastilla + Jolee + Mission + Zaalbar/JML + JKR + Bastilla + Jolee\n\n\
+        **Guarded Assault :** Complete the battle without losing a unit while at least one Attacker is in the field\n\t- JMK CAT\n\n',
         # Boss Feats
         'Boss':
-        '**Boss : DR:**\n\
-        **Hello There :** Win with Jedi Knight Anakain and General Kenobi surviving\n\t- JML/JMK/Padme CAT\n\n\
-        **All Clone Troopers :** Win with a full Squad of Clone Troopers units\n\t- F that\n\n',
+        '**Boss : JKR:**\n\
+        **Lure of the Light :** Win with Darth Revan and Bastilla Shan (Fallen) surviving\n\t- Dralak\n\n\
+        **Hokey Religions :** Win without using any Jedi, Sith, or Unaligned Force User in your Squad\n\t- Whatever\n\n',
         },
     # Sector 4
     '4':{
@@ -101,14 +101,13 @@ FEATS = {
     # General Feats
     'g':{
         'General':
-        '**Live in Light :** Win 50 battles with a full Squad of Light Side units\n\t- Whatever\n\n\
-        **Dabble in Darkness :** Win 50 battles with a full Squad of Dark Side units\n\t- GG Sector 3/Sith/IT Sector 2\n\n\
+        '**I Have Broken My Chains :** Win 20 battles without using a Leader in the leader slot.\n\t- CAT JMK/C3PO + Sion + Nihilus Boss Sector 2\n\n\
+        **Sustained by Rage :** Defeat 100 enemies with Darth Malak or Darth Revan\n\t- Dralak\n\n\
         **Grand Victory :** Defeat at least 500 enemies\n\t- Whatever\n\n\
-        **I Want My Armor :** Win 20 battles with Beskar Mando & Boba Fett\n\t- In Sector 1 : BH + Bando + Boba\n\n\
-        **Great Shot Kid :** Gain Critical Over 20 Times\n\t- Requires completing A New Hope Feat\n\n\
-        **A New Hope :** Win 20 battles with Farmboy Luke and Old Ben\n\t- Rebels/JML + Old Ben + Zariss Sector 2\n\n\
-        **Unlimited Power :** Gain Massively Overpowered 20 Times\n\t- Requires completing I Know There is Good in You Feat\n\n\
-        **I Know There is Good in You :** Defeat 100 enemies with Darth vader or Jedi Knight Luke Skywalker\n\t- Sith/Empire/Jedi\n\n\
-        **Grand Conquest :** Win 250 Battles\n\t- Whatever\n\n',
+        **Measure of Admiration :** Win 40 battles with a full squad of Old Republic\n\t- JKR Bastilla Jolee Zaalbar Mission + Zealous Ambition Data Disk in Sector 1\n\n\
+        **A New Empire :** Win 40 battles with a full squad of Sith Empire\n\t- Dralak\n\n\
+        **War Made Manifest :** Win 20 battles with Darth Nihilus and Darth Sion in your squad\n\t- C3PO + Sion + Nihilus Boss Sector 2\n\n\
+        **Visions of Doubt :** Gain Foresight from Premonition 20 times\n\t- Requires completing I Have Broken My Chains Feat\n\n\
+        **Power of the Dark Side :** Defeat enemies with Dread 20 times\n\t- Requires completing Sustained by Rage Feat\n\n\,
         },
 }
